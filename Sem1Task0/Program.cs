5897d9ec51df0268eedfa1d7a8b9d45ab43fe472
Tatiana <mumrenysh@gmail.com>
2022-12-02 11:47:22 +0300

    add solusion for task 0,1,3,5,7

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,11 @@
+﻿string num1Line = Console.ReadLine();
+string num2Line = Console.ReadLine();
+
+if(num1Line!=null&&num2Line!=null)  //проверили, что числа не нулевые, что есть значение
+{
+    int num1 = int.Parse(num1Line); // оператор Parse превращает наше число из строки в десятичное
+    int num2 = int.Parse(num2Line);
+
+    int outResult = num1*num2;
+    Console.WriteLine(outResult);
+}
---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine!=null)
+{
+    string[] dayOfWeek = new string[7];
+    dayOfWeek[0] = "понедельник";
+    dayOfWeek[1] = "вторник";
+    dayOfWeek[2] = "среда";
+    dayOfWeek[3] = "четверг";
+    dayOfWeek[4] = "пятница";
+    dayOfWeek[5] = "суббота";
+    dayOfWeek[6] = "воскресенье";
+
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputLine);
+
+    //Находим название дня недели по введенному номеру
+    string outDayOfWeek = dayOfWeek[inputNumber-1];
+
+    //Выводим данные в консоль
+    Console.WriteLine(outDayOfWeek);
+}
+
+   
