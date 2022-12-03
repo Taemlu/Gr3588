afbfb0e8291dba79806716551afad6c42fa918d1
Tatiana <mumrenysh@gmail.com>
2022-12-03 15:41:53 +0300

    task for 1.2, 2.9, 2.11, 2.14, 2.16 added

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿//создаем экземпляр класса рандомайзер
+System.Random numberSintezator = new System.Random();
+
+//получаем новое слуйчаное число
+int num = numberSintezator.Next(100,1000);
+
+Console.WriteLine(num);
+
+// int number1 = num/100; //получаем старший разряд числа
+// int number2 = num%10; //получаем младщий разряд числа
+
+//int result = (num/100)*10 + num%10; //ввели сразу выражения вместо number1,2
+Console.WriteLine((num/100)*10 + num%10); //записали сразу выражение в результат
---
+++ Sem2Task9/Program.cs
@@ -0,0 +1,32 @@
+﻿// System.Random numberSintezator = new System.Random(); // создадим экземпляр класса рандомайзер
+
+// int number = numberSintezator.Next(10,100); //Получаем новое случайное число
+
+// Console.WriteLine(number);
+
+// int firstNumber = number/10;
+// int secondNumber = number%10; //получаем первое и второе число по отдельности      
+
+// //сравниваем числа
+// if (firstNumber>secondNumber)
+// {
+//     Console.WriteLine(firstNumber);
+// }
+// else
+// {
+//    //выводим данные в консоль
+//    Console.WriteLine(secondNumber);
+// }
+//вариант 2
+char[] digits = numberSintezator.Next(10,100).ToString().ToCharArray();
+Console.WriteLine(digits);
+
+firstNumber = ((int)digits[0])-48;
+secondNumber = ((int)digits[1])-48;
+
+int resultNumber = firstNumber>secondNumber?resultNumber=firstNumber:resultNumber=secondNumber;
+Console.WriteLine(resultNumber);
+
+
+
+
