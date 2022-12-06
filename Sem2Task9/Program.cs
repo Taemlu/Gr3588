f11d8fce0c55b7b4a78f6f24a46f12f52769e134
Tatiana <mumrenysh@gmail.com>
2022-12-06 12:01:04 +0300

    Task9 - mistakes are fixed

--- Sem2Task9/Program.cs
+++ Sem2Task9/Program.cs
@@ -1,31 +1,31 @@
-﻿// System.Random numberSintezator = new System.Random(); // создадим экземпляр класса рандомайзер
+﻿System.Random numberSintezator = new System.Random(); // создадим экземпляр класса рандомайзер
 
-// int number = numberSintezator.Next(10,100); //Получаем новое случайное число
+int randomNumber = numberSintezator.Next(10,100); //Получаем новое случайное число
 
-// Console.WriteLine(number);
+Console.WriteLine(randomNumber);
 
-// int firstNumber = number/10;
-// int secondNumber = number%10; //получаем первое и второе число по отдельности      
+int firstNumber = randomNumber/10;
+int secondNumber = randomNumber%10; //получаем первое и второе число по отдельности      
 
-// //сравниваем числа
-// if (firstNumber>secondNumber)
-// {
-//     Console.WriteLine(firstNumber);
-// }
-// else
-// {
-//    //выводим данные в консоль
-//    Console.WriteLine(secondNumber);
-// }
-//вариант 2
-char[] digits = numberSintezator.Next(10,100).ToString().ToCharArray();
-Console.WriteLine(digits);
+//сравниваем числа
+if (firstNumber>secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+   //выводим данные в консоль
+   Console.WriteLine(secondNumber);
+}
+// //вариант 2
+// char[] digits = numberSintezator.Next(10,100).ToString().ToCharArray();
+// Console.WriteLine(digits);
 
-firstNumber = ((int)digits[0])-48;
-secondNumber = ((int)digits[1])-48;
+// int firstNumber = ((int)digits[0])-48;
+// int secondNumber = ((int)digits[1])-48;
 
-int resultNumber = firstNumber>secondNumber?resultNumber=firstNumber:resultNumber=secondNumber;
-Console.WriteLine(resultNumber);
+// int resultNumber = firstNumber>secondNumber?resultNumber=firstNumber:resultNumber=secondNumber;
+// Console.WriteLine(resultNumber);
 
 
 
