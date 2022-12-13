0ce22f58799b0cc92f6e1e83283e11cdcd923c6b
Tatiana <mumrenysh@gmail.com>
2022-12-13 09:50:04 +0300

    solution for 19, 21, 23 is added

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+// Метод вывода данных
+void PrintData(bool result)
+{
+    Console.WriteLine(result);
+}
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int number = ReadData("Введите пятизначное число: ");
+bool result = PalinTest(number);
+PrintData(result);
+bool PalinTest(int number)
+{
+    bool result = false;
+    result = (number/10000==number%10&&(number/1000)%10==(number/10)%10);
+
+    return result;
+}
+
+
