9de1f2ca584ed17294ad00708d596a0c52fd2378
Tatiana <mumrenysh@gmail.com>
2022-12-16 14:03:29 +0300

    solutions for 25,27,29 are added

---
+++ Sem3Task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// //Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(int res)
+{
+    Console.WriteLine(res);
+}
+
+
+int Pow(int numA, int numB)
+{
+  int res = 1;
+  for(int i=1; i <= numB; i++)
+  {
+    res = res * numA;
+  }
+    // int result = Math.Pow(numberA, numberB);
+    return res;
+}
+
+int num1 = ReadData("Введите число А:");
+int num2 = ReadData("Введите число B:");
+
+int result = Pow(num1, num2);
+PrintData(result);
---
+++ Sem3Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(int res)
+{
+    Console.WriteLine(res);
+}
+
+int SumDigit(int num)
+{
+    int res = 0;
+    while (num > 0)
+    {
+        res+=num%10;
+        num=num/10;
+
+    }
+    return res;
+}
+int number = ReadData("Введите число:");
+int result = SumDigit (number);
+PrintData(result);
